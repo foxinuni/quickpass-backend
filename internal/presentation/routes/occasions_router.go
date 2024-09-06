f96b3a21ef324214f6189e69ab9579530bbc1216
DownloadableFox <me@downloadablefox.dev>
2024-09-05 23:44:16 -0500

    fixed dumb mistake where occasions router needed auth

--- internal/presentation/routes/occasions_router.go
+++ internal/presentation/routes/occasions_router.go
@@ -2,23 +2,20 @@ package routes
 
 import (
 	"github.com/foxinuni/quickpass-backend/internal/presentation/controllers"
-	"github.com/foxinuni/quickpass-backend/internal/presentation/middlewares"
 	"github.com/labstack/echo/v4"
 )
 
 type OccasionsRouter struct {
 	occasionsController *controllers.OccasionsController
-	authStrategy        middlewares.AuthStrategy
 }
 
-func NewOccasionsRouter(occasionsController *controllers.OccasionsController, authStrategy middlewares.AuthStrategy) *OccasionsRouter {
+func NewOccasionsRouter(occasionsController *controllers.OccasionsController) *OccasionsRouter {
 	return &OccasionsRouter{
 		occasionsController: occasionsController,
-		authStrategy:        authStrategy,
 	}
 }
 
 func (or *OccasionsRouter) RegisterRoutes(echo *echo.Echo) {
 	// Register the create occasion route
-	echo.POST("/occasions", or.occasionsController.Create, middlewares.AuthMiddleware(or.authStrategy))
+	echo.POST("/occasions", or.occasionsController.Create)
 }
