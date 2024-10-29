356fd80f0fbbab1d0e47940260baf76ff1c86210
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-10-28 22:51:23 -0500

    check session to see if its enabled endpoint implemented

--- internal/domain/services/session_service.go
+++ internal/domain/services/session_service.go
@@ -8,6 +8,7 @@ import (
 type SessionService interface {
 	GetAllSessions() ([]*entities.Session, error)
 	EnableSession(sessionID int, state bool) error
+	CheckSession(token string) (bool, error)
 }
 
 type RepoSessionService struct {
@@ -22,6 +23,14 @@ func NewRepoSessionService(sessionRepo repo.SessionRepository) SessionService {
 
 func (s *RepoSessionService) GetAllSessions() ([]*entities.Session, error) {
 	return s.sessionRepo.GetAll()
+}
+
+func  (s *RepoSessionService) CheckSession(token string) (bool, error){
+	session, err := s.sessionRepo.GetByToken(token)
+	if err != nil{
+		return false, err
+	}
+	return session.Enabled, nil
 }
 
 func (s *RepoSessionService) EnableSession(sessionID int, state bool) error {
